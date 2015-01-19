04d5d4393b1a460e9de6085e9cd76ace93233122
Yannick <github@yannick.md>
2015-01-20 00:15:15 +0100

    tries to fix cors

--- js/script.js
+++ js/script.js
@@ -11,29 +11,31 @@ var app = {
   },
   getRate: function($el) {
     $.ajax({
-      url: 'http://blockchain.info/ticker?cors=true',
-      crossDomain: true,
-    }).done(function(data) {
-      var rate = data.EUR.last,
-          oldRate = parseFloat($el.text()),
-          suffix = '';
+      url: 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%27http%3A%2F%2Fblockchain.info/ticker%27%0A&format=json&callback=?',
+      dataType: 'jsonp',
+      success: function(data) {
+        var json = $.parseJSON(data.query.results.body.p),
+            rate = data.EUR.last,
+            oldRate = parseFloat($el.text()),
+            suffix = '';
 
-      if (rate > oldRate) {
-        suffix = '➚';
+        if (rate > oldRate) {
+          suffix = '➚';
+        }
+        else if (rate < oldRate) {
+          suffix = '➘';
+        }
+  
+        if (suffix) {
+          suffix = ' ' + suffix;
+        }
+  
+        document.title = rate + ' ' + data.EUR.symbol + suffix;
+        $el.text(rate);
       }
-      else if (rate < oldRate) {
-        suffix = '➘';
-      }
-
-      if (suffix) {
-        suffix = ' ' + suffix;
-      }
-
-      document.title = rate + ' ' + data.EUR.symbol + suffix;
-      $el.text(rate);
     });
   }
-}
+};
 
 $(document).ready(function() {
   app.init();
