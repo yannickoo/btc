09442acd6c5d770acd0f08ed46f907d36be9ab74
Yannick <github@yannick.md>
2015-01-28 09:53:44 +0100

    gets rid of yql, fixes #1

--- js/script.js
+++ js/script.js
@@ -10,29 +10,24 @@ var app = {
     }, 60 * 1000);
   },
   getRate: function($el) {
-    $.ajax({
-      url: 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%27http%3A%2F%2Fblockchain.info/ticker%27%0A&format=json&callback=?',
-      dataType: 'jsonp',
-      success: function(data) {
-        var json = $.parseJSON(data.query.results.body.p),
-            rate = json.EUR.last,
-            oldRate = parseFloat($el.text()),
-            suffix = '';
+    $.getJSON('https://blockchain.info/ticker?cors=true', function(data) {
+      var rate = data.EUR.last,
+          oldRate = parseFloat($el.text()),
+          suffix = '';
 
-        if (rate > oldRate) {
-          suffix = '➚';
-        }
-        else if (rate < oldRate) {
-          suffix = '➘';
-        }
-  
-        if (suffix) {
-          suffix = ' ' + suffix;
-        }
-  
-        document.title = rate + ' ' + json.EUR.symbol + suffix;
-        $el.text(rate);
+      if (rate > oldRate) {
+        suffix = '➚';
       }
+      else if (rate < oldRate) {
+        suffix = '➘';
+      }
+  
+      if (suffix) {
+        suffix = ' ' + suffix;
+      }
+  
+      document.title = rate + ' ' + data.EUR.symbol + suffix;
+      $el.text(rate);
     });
   }
 };
